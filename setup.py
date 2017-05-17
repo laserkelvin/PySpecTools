e3226691d1d7e1ed7db0c149b0295945c05b24b8
Kelvin Lee <kin.long.kelvin.lee@gmail.com>
2017-05-17 14:59:52 -0400

    Fixed the setup scripts for testing. Should be functional.

--- setup.py
+++ setup.py
@@ -1,8 +1,33 @@
 from setuptools import setup
+from setuptools.command.install import install
+from glob import glob
+import os
+import stat
+import sys
+
+class PostInstallCommand(install):
+    def run(self):
+        format_dict = {"python_path": sys.executable}
+        if os.path.isdir(os.path.expanduser("~") + "/bin") is False:
+            os.mkdir(os.path.expanduser("~") + "/bin")
+
+        templates = glob("./scripts/*")
+        if len(templates) == 0:
+            pass
+        else:
+            for template in templates:
+                template_name = template.split("/")[-1]
+                with open(template, "r") as read_file:
+                    file_contents = read_file.read()
+                with open(os.path.expanduser("~") + "/bin/" + template_name, "w+") as write_file:
+                    write_file.write(file_contents.format(**format_dict))
+                os.chmod(os.path.expanduser("~") + "/bin/" + template_name,
+                         stat.S_IRWXU | stat.S_IRWXG | stat.S_IROTH)
+        install.run(self)
 
 setup(
     name="pyspectools",
-    version="0.1",
+    version="0.2.0",
     description="A set of Python tools/routines for spectroscopy",
     author="Kelvin Lee",
     packages=["pyspectools"],
@@ -13,6 +38,11 @@ setup(
             "pandas",
             "scipy",
             "colorlover",
-            "matplotlib"
-    ]
+            "matplotlib",
+            "peakutils"
+    ],
+    cmdclass={
+        "develop": PostInstallCommand,
+        "install": PostInstallCommand
+    }
 )
