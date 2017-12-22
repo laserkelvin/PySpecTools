810e0cfbd110b06874e3c70d02f038d6d64c124b
Kelvin Lee <kin.long.kelvin.lee@gmail.com>
2017-12-22 13:05:47 -0500

    Rewrote second derivative lineshape
    Rather than having weird constants, I've now included pi and whatnot.

--- pyspectools/lineshapes.py
+++ pyspectools/lineshapes.py
@@ -8,12 +8,15 @@ import numpy as np
     are explicitly coded are Gaussian and Lorentizan functions,
     and derivatives of these are calculated analytically
     using SymPy.
+
+    When available, use the lmfit built-in functions for
+    lineshapes.
 """
 
-def gaussian(x, A, c, w):
+def gaussian(x, A, x0, w):
     # stock Gaussian distribution. The sign of A is such that the function
     # is flipped upside down.
-    return A * np.exp(-(c - x)**2. / 2. * w**2.)
+    return A * np.exp(-(x - x0)**2. / 2. * w**2.)
 
 
 def lorentzian(x, x0, gamma, A):
@@ -26,18 +29,15 @@ def first_deriv_lorentzian(x, x0, gamma, A):
         Analytic first derivative function for a Lorentizan.
         This was solved using SymPy.
     """
-    i = 0.63661977236758
-    return ((i * A * gamma**3.0) * (x - x0)) / (gamma**2. + (x - x0)**2.)**2.
+    return (-2. * A * gamma**3. * (x - x0)) / (np.pi * (gamma**2. + (x - x0)**2.)**2.)
 
 
 def sec_deriv_lorentzian(x, x0, gamma, A):
     """
         Analytic second derivative function for a Lorentizan.
-
+        Similar to the first derivative, this was derived using SymPy.
         The function is split into two parts K and L.
     """
-    i = 0.63661977236758
-    j = 2.5464790894703
-    K = -(i * A * gamma**3.) / (gamma**2. + (x - x0)**2.)**2.
-    L = (j * A * gamma**3. * (x - x0)**2.) / (gamma**2. + (x - x0)**2.)**3.
+    K = (-2. * A * gamma**3.) / (np.pi * (gamma**2. + (x - x0)**2.)**2.)
+    L = (8. * A * gamma**3. * (x - x0)**2.) / (np.pi (gamma**2. + (x - x0_**2.)**3.))
     return K + L
