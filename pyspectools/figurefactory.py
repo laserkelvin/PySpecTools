dd48e0c1596bf2fd019e88679e828436d29646f5
Kelvin Lee <kin.long.kelvin.lee@gmail.com>
2018-03-06 10:16:46 -0500

    Added PES drawing routine
    Copy pasted from earlier code that will generate
    a smooth PES

--- pyspectools/figurefactory.py
+++ pyspectools/figurefactory.py
@@ -3,6 +3,7 @@ import matplotlib as mpl
 from cycler import cycler
 from matplotlib import pyplot as plt
 from matplotlib.offsetbox import AnnotationBbox, OffsetImage
+import numpy as np
 
 mpl.style.use("seaborn")
 
@@ -37,6 +38,30 @@ def no_scientific(axis):
         - Generic energy diagrams
         - Adding images to a matplotlib figure
 """
+
+def make_pes(x, energies, width=5):
+    """ Function to create a smooth PES, where stationary 
+        points are connected.
+        
+        Basically makes a linearly spaced x array which
+        pads x-axis density to create the horizontal line
+        for the stationary points
+        
+        Optional arguments are how many x-points to 
+        pad with on either side.
+    """
+    new_x = np.array([])
+    new_energies = list()
+    for xvalue, energy in zip(x, energies):
+        new_x = np.append(
+                new_x, 
+                np.linspace(
+                    xvalue - (width * 0.05), 
+                    xvalue + (width * 0.05), 
+                    width * 2)
+                )
+        new_energies.append([energy] * width * 2)
+    return new_x, np.array(new_energies).flatten()
 
 
 def calc_vibE(quant_nums, vibrations):
