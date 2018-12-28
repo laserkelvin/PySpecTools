71dd9df8287468d34862581f1d2d4aa5698b7c67
Kelvin Lee <kin.long.kelvin.lee@gmail.com>
2018-12-28 10:13:42 -0500

    Code cleanup

--- pyspectools/routines.py
+++ pyspectools/routines.py
@@ -7,9 +7,11 @@ import os
 import subprocess
 import shutil
 import json
-import yaml
-from pyspectools import pypickett as pp
 from glob import glob
+
+import yaml
+
+from pyspectools import pypickett as pp
 
 import yaml
 import joblib
