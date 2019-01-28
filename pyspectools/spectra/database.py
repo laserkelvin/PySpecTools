792d1657fc10dc68c7d5ddf841b15509b3bdeb8f
Kelvin Lee <kin.long.kelvin.lee@gmail.com>
2019-01-28 17:12:16 -0500

    Started on database class wrapping tinydb

--- pyspectools/spectra/database.py
+++ pyspectools/spectra/database.py
@@ -6,4 +6,21 @@
     TODO - set up routines for a persistent database
 """
 
+import os
 
+import tinydb
+
+
+class SpectralCatalog(tinydb.TinyDB):
+    def __init__(self, dbpath=None):
+        if dbpath is None:
+            dbpath = os.path.expanduser("~/.pyspectools/pyspec.db")
+        super().__init__(dbpath)
+
+    def search_frequency(self, frequency):
+        """
+        TODO make a frequency look up function
+        :param frequency:
+        :return:
+        """
+        return None
