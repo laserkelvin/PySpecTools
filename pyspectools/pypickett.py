42e0d9165409e2853a24a7106cb4c3c3412ea05e
Kelvin <kin.long.kelvin.lee@gmail.com>
2019-05-07 10:32:06 -0400

    Fixed bracket typo

--- pyspectools/pypickett.py
+++ pyspectools/pypickett.py
@@ -992,7 +992,7 @@ class AutoFitSession:
             # Parse the output
             fit_dict = parsers.parse_fit(self.filename + ".fit")
             # If the RMS is improved upon, and there is more than one line fit
-            if (fit_dict["rms"] < self.rms) or (len(fit_dict["o-c"] >= self.nfit)):
+            if (fit_dict["rms"] < self.rms) or (len(fit_dict["o-c"]) >= self.nfit):
                 if fit_dict["rms"] > 0.001:
                     with open("{}.par".format(self.filename), "r") as read_file:
                         self.par = read_file.readlines()
