Fixed masking issue again

pyspectools/spectra/assignment.py
@@ -1406,7 +1406,7 @@ class AssignmentSession:
             intensities = np.array([uline.intensity for uline in ulines])
             mask = np.where(intensities >= thres)
             dr_freqs = np.asarray(dr_freqs)[mask]
-            cavity_freqs = np.asarray(cavity_freqs)[mask]
+            cavity_freqs = np.asarray(cavity_freqs)
         lines = ""
         for cindex, cavity in enumerate(cavity_freqs):
             for dindex, dr in enumerate(dr_freqs):