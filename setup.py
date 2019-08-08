6bc910e6413731a83f4ce5051f06e4c50126fc51
Kelvin <kin.long.kelvin.lee@gmail.com>
2019-08-08 08:52:55 -0400

    Rolling into version 4.1.2

--- setup.py
+++ setup.py
@@ -180,7 +180,7 @@ cmdclass.update(
 
 setup(
     name="pyspectools",
-    version="4.1.1",
+    version="4.1.2",
     description="A set of Python tools/routines for spectroscopy",
     author="Kelvin Lee",
     packages=find_packages(),
