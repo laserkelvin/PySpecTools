8aa0a7c36db993d9ba04321ba701edbc10f994ee
Kelvin Lee <kin.long.kelvin.lee@gmail.com>
2020-02-26 09:40:44 -0500

    Cleaned up examples folder and created tests
    First batch of tests is created for the assignment module.
    There are currently three tests that ensure the same level
    of functionality, and that everything is functioning as
    intended.

--- tests/generate_test_spectrum.py
+++ tests/generate_test_spectrum.py
@@ -1,4 +1,3 @@
-
 """
 generate_test_spectrum.py
 
@@ -7,6 +6,9 @@ Script to generate a spectrum for testing PySpecTools.
 This uses the `pyspectools.fast.gaussian` to generate Gaussian
 peaks at uniform random locations. The amplitudes are sampled
 from a normal distribution.
+
+Every peak is grouped together using random integers; these
+will be used later to test the `assignment` module mechanisms.
 """
 
 from pyspectools.fast.lineshapes import gaussian
@@ -15,18 +17,27 @@ import pandas as pd
 import yaml
 
 
+SEED = 42
 NPEAKS = 200
+NGROUPS = 10
 RESOLUTION = 0.02
-FREQ_RANGE = (8000., 19000.)
+FREQ_RANGE = (8000.0, 19000.0)
+
+# Set a specific random seed to get consistent results
+_ = np.random.seed(SEED)
 
 # Gaussian parameters
-AMPS = np.random.normal(loc=5., scale=2., size=(NPEAKS))
-CENTERS = np.random.uniform(low=min(FREQ_RANGE) + 50., high=max(FREQ_RANGE) - 50., size=(NPEAKS))
+AMPS = np.random.normal(loc=5.0, scale=2.0, size=(NPEAKS))
+CENTERS = np.random.uniform(
+    low=min(FREQ_RANGE) + 50.0, high=max(FREQ_RANGE) - 50.0, size=(NPEAKS)
+)
 # 50 kHz sigma
 WIDTH = 0.05
+# This generates encodings for each of the peaks as fake assignments
+ASSIGNMENTS = np.random.randint(low=0, high=NGROUPS, size=(NPEAKS))
 
 X = np.arange(min(FREQ_RANGE), max(FREQ_RANGE), RESOLUTION)
-Y = np.random.normal(loc=0., scale=0.2, size=(X.size))
+Y = np.random.normal(loc=0.0, scale=0.2, size=(X.size))
 Y = np.abs(Y)
 
 # Loop over each of parameters, and generate a Gaussian
@@ -45,7 +56,8 @@ param_dict = {
     "FREQ_RANGE": FREQ_RANGE,
     "CENTERS": np.round(CENTERS, 4).tolist(),
     "AMPS": np.round(AMPS, 4).tolist(),
-    "WIDTH": WIDTH
+    "ASSIGNMENTS": ASSIGNMENTS.tolist(),
+    "WIDTH": WIDTH,
 }
 with open("spectrum-parameters.yml", "w+") as write_file:
     yaml.dump(param_dict, write_file)
