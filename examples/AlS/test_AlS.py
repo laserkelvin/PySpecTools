4aa882e30bfe145f251fc3bcb555deedcb444364
Kelvin Lee <kin.long.kelvin.lee@gmail.com>
2020-02-26 09:40:44 -0500

    Cleaned up examples folder and created tests
    First batch of tests is created for the assignment module.
    There are currently three tests that ensure the same level
    of functionality, and that everything is functioning as
    intended.

--- examples/AlS/test_AlS.py
+++
@@ -1,8 +0,0 @@
-from pyspectools import routines
-
-AlS = routines.pickett_molecule("AlS.json")
-
-AlS.setup_par()
-AlS.setup_int()
-AlS.predict_lines()
-
--- examples/HCN/HCN_test.py
+++
@@ -1,7 +0,0 @@
-from pyspectools import routines
-
-hcn = routines.pickett_molecule("HCN.json")
-
-hcn.setup_par()
-hcn.setup_int()
-
