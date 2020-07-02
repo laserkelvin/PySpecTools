0bec4f92a67746a3f4f3a04a560b2f13532d75af
Kelvin Lee <kin.long.kelvin.lee@gmail.com>
2020-07-02 14:30:58 -0400

    Performance improvements on non-Cython lineshapes
    Turns out some of the lineshapes in `fast` are broken for
    whatever reason. Instead of fixing the Cython code, it's
    probably more beneficial to just provide JIT'd code.
    
    `pyspectools.lineshapes` is now optimized with Numba JIT,
    with fairly reasonable performance on large arrays, and with
    large numbers of multiple Gaussians. From this point we will
    begin to depreciate the Cython routines in favor of JIT

--- pyspectools/lineshapes.py
+++ pyspectools/lineshapes.py
@@ -1,4 +1,5 @@
 import numpy as np
+import numba
 
 """
     lineshapes.py
@@ -13,27 +14,63 @@ import numpy as np
 """
 
 
-def gaussian(x, A, x0, w):
-    # stock Gaussian distribution. The sign of A is such that the function
-    # is flipped upside down.
+@numba.jit(fastmath=True, nopython=True)
+def gaussian(x: np.ndarray, A=1., x0=0., w=1.):
+    """
+    Vectorized implementation of a single Gaussian lineshape. For a given
+    array of `x` values, we compute the corresponding amplitude of a
+    Gaussian for a set of amplitude, center, and widths.
+    
+    Uses JIT compilation with Numba, and should be reasonably fast.
+
+    Parameters
+    ----------
+    x : np.ndarray
+        NumPy 1D array containing x values to evaluate over.
+    A : float
+        Scaling of the Gaussian; actually corresponds to the area.
+    x0 : float
+        Center of the Gaussian
+    w : float
+        Width of the Gaussian.
+
+    Returns
+    -------
+    NumPy 1D array
+        Array of amplitude values of the specified Gaussian
+    """
     return A * np.exp(-(x - x0) ** 2.0 / (2.0 * w ** 2.0))
 
 
-def pair_gaussian(x, A1, A2, x0, w, xsep):
+@numba.jit(fastmath=True, nopython=True)
+def pair_gaussian(x: np.ndarray, A1: float, A2: float, x0: float, w: float, xsep: float):
     """
     Paired Gaussian lineshape. The function allows for the amplitudes to be
     floated, however the two Gaussians are locked in width and center
     frequency.
-    :param x: np.array to evaluate the pair gaussians on
-    :param A1, A2: float amplitude of the gaussians
-    :param x0: float center-of-mass value for the two gaussian centers
-    :param w: float width of the gaussians
-    :param xsep: float seperation expected for the gaussian centers
-    :return: np.array y values of the gaussian
+
+    Parameters
+    ----------
+    x : np.ndarray
+        [description]
+    A1, A2 : float
+        Amplitude of the two Gaussians
+    x0 : float
+        Centroid of the two Gaussians
+    w : float
+        Width of the two Gaussians
+    xsep : float
+        Distance from the centroid and a Gaussian center
+
+    Returns
+    -------
+    NumPy 1D array
+        Array of amplitude values of the pair of Gaussians
     """
     return gaussian(x, A1, x0 - xsep, w) + gaussian(x, A2, x0 + xsep, w)
 
 
+@numba.jit(fastmath=True, nopython=True)
 def lorentzian(x, x0, gamma, I):
     """
     Function to evaluate a Lorentzian lineshape function.
@@ -57,6 +94,7 @@ def lorentzian(x, x0, gamma, I):
     return I * (gamma ** 2.0 / ((x - x0) ** 2.0 + gamma ** 2.0))
 
 
+@numba.jit(fastmath=True, nopython=True)
 def first_deriv_lorentzian(x, x0, gamma, I):
     """
     Function to evaluate the first derivative of a Lorentzian lineshape function.
@@ -86,6 +124,7 @@ def first_deriv_lorentzian(x, x0, gamma, I):
     )
 
 
+@numba.jit(fastmath=True, nopython=True)
 def sec_deriv_lorentzian(x, x0, gamma, I):
     """
     Function to evaluate the second derivative of a Lorentzian lineshape function.
@@ -114,3 +153,44 @@ def sec_deriv_lorentzian(x, x0, gamma, I):
         * (2.0 - 8.0 * (x - x0) ** 2.0 / (gamma ** 2.0 + (x - x0) ** 2.0))
         / (gamma ** 2.0 + (x - x0) ** 2.0) ** 2
     )
+
+
+@numba.jit(fastmath=True, nopython=True, parallel=True, nogil=True)
+def fast_multi_gaussian(x: np.ndarray, A: np.ndarray, x0: np.ndarray, w: np.ndarray):
+    """
+    Fast, parallel implementation of a mixture of Gaussian lineshapes.
+    Uses the `gaussian` function defined in this module, which itself is
+    also JIT'd, and uses parallel loops to evaluate multiple Gaussians.
+    
+    The result is a NumPy 2D array of shape N x D, where N is the length
+    of the frequency array, and D is the number of Gaussians.
+    
+    The inputs are expected to all be NumPy arrays, where A, x0, and w
+    all equal in length and contain parameters for each respective Gaussian.
+    
+    With a 200,000 length frequency array and about 70 Gaussians, this code
+    takes ~70 ms to compute; about four times faster than the unJIT'd version.
+
+    Parameters
+    ----------
+    x : np.ndarray
+        [description]
+    A : np.ndarray
+        [description]
+    x0 : np.ndarray
+        [description]
+    w : np.ndarray
+        [description]
+
+    Returns
+    -------
+    [type]
+        [description]
+    """
+    assert A.size == x0.size == w.size
+    ngaussians = len(A)
+    y = np.zeros((x.size, ngaussians))
+    # parallelize the loop over number of Gaussians
+    for i in numba.prange(ngaussians):
+        y[:,i] = gaussian(x, A[i], x0[i], w[i])
+    return y
