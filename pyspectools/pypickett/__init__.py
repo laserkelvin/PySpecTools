92656aa1e6612b199a653abd7738d4b8ec7c741e
Kelvin Lee <kin.long.kelvin.lee@gmail.com>
2021-09-30 11:12:57 -0400

    feat: added function for parsing molecule yaml

--- pyspectools/pypickett/__init__.py
+++ pyspectools/pypickett/__init__.py
@@ -1,3 +1,4 @@
 
 from pyspectools.pypickett import classes, utils
 from pyspectools.pypickett.classes import AsymmetricTop, SymmetricTop, LinearMolecule, SPCAT
+from pyspectools.pypickett.utils import load_molecule_yaml
