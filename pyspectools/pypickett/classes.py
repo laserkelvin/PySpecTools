7fa4f3bd10cb5e4c7f9545e523427c6b67fb6328
Kelvin Lee <kin.long.kelvin.lee@gmail.com>
2021-10-01 10:39:17 -0400

    feat: infer var settings from Hamiltonian terms used

--- pyspectools/pypickett/classes.py
+++ pyspectools/pypickett/classes.py
@@ -740,4 +740,10 @@ def load_molecule_yaml(
             f"""Unsupported parameters in YAML; keys: {", ".join(unsupported)}"""
         )
     molecule = mol_type(**data)
-    return (molecule, metadata, mu)
+    # infer the reduction from the keys specified
+    var_kwargs = {
+        "mu": mu,
+        "s_reduced": True if "DJ" in data else False,
+        "prolate": False if mol_type.__name__ == "AsymmetricTop" else True
+    }
+    return (molecule, metadata, var_kwargs)
