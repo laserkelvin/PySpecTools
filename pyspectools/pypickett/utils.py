545ba97914cd232c2931a170b7d8f8d615682600
Kelvin Lee <kin.long.kelvin.lee@gmail.com>
2023-09-07 12:47:08 -0400

    modifying template for par and int files

--- pyspectools/pypickett/utils.py
+++ pyspectools/pypickett/utils.py
@@ -13,12 +13,12 @@ from pyspectools import routines
 
 par_template = """PySpecTools SPCAT input
  100  255    1    0    0.0000E+000    1.0000E+003    1.0000E+000 1.0000000000
-{reduction}   {quanta}    {top}    0   {k_max}    0    {weight_axis}    {even_weight}    {odd_weight}     0   1   0
+{reduction}   {quanta}    {top}    0   {k_max}    0    {weight_axis}    {even_weight}    {odd_weight}     0   1  3
 {parameters}
 """
 
 int_template = """PySpecTools SPCAT input
- 0  {mol_id}   {q:.4f}   0   {max_f_qno}  {int_min:.1f}  {int_max:.1f}   {freq_limit:.4f}  {T:.2f}
+ 1  {mol_id}   {q:.4f}   0   {max_f_qno}  {int_min:.1f}  {int_max:.1f}   {freq_limit:.4f}  {T:.2f}
 {dipole_moments}
 """
 
